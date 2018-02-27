a835889582921d803686ac28bf4fc356583ef8a3
Meg Scholl <meghan.e.scholl@gmail.com>
2018-02-27 17:39:43 -0600

    added new comments for code plan i.e icon for home, form, entering new data, and a for loop to loop through cities and restaurants

--- foodie-list.js
+++ foodie-list.js
@@ -1,6 +1,77 @@
 "use strict";
+/*
+Your job is to build a web page where a user can see all the restaurants they have been to and filter by city. They can also add additional restaurants.
+
+// SELECT DROP-DOWN TAGS
+// USE JSON FILES
+// FILTER ABILITIES
+// XMLHttpRequest() with PROMISES
+// DIV'S TO BUILD REPRESENTATIONS OF DATA
+// FORM TO ENTER NEW RESTAURANT INFO
+
+// KEEP WEBSITE CLEAN & SIMPLE -- USE CARDS / GRID 
+*/
+
+
+//
+//
+//
+
 
 /*
-The two JSON representations above should be in two files. 
-Use Javascript's XMLHttpRequest() method with Promises to read the data from those files, and then build HTML representations of the data.
+The two JSON representations above should be in two files. ** DONE **
+*/
+
+/*
+Use Javascript's XMLHttpRequest() method with Promises to read the data from those files, and then build HTML representations of the data. ** NEED TO DO **
+
+FUNCTION FOR XMLHTTPREQUEST - FIREBASE - WITH PROMISES
+FUNCTION WILL BUILD REPRESENTATION OF DATA
+*/
+
+
+/*
+When the user first visits the page, all restaurants should be listed in order of their rating (ratings are from 1 to 10). The restaurants are displayed from highest rated to lowest rated.
+
+FUNCTION TO RUN THROUGH RATINGS
+*/
+
+
+
+
+
+/*
+There should be a <select> element on the page with the list of cities. When a user selects a city, the restaurants displayed should be filtered so that only the restaurants from the selected city are displayed. Make sure there is an option for All so that a user can go back to viewing all the restaurants.
+
+FUNCTION TO FILTER AND DISPLAY SELECTED CITY'S RESTAURANTS
+IF ELSE - OPTION FOR ALL
+*/
+
+
+
+
+
+/*
+If the user selects Nashville, there should an icon or text that appears on the page that shows the user that this is their hometown.
+
+ICON FOR HOME (NASHVILLE)
+
+*/
+
+
+
+
+
+
+
+/*
+There should be a form for a user to add more restaurants with a <select> element for picking which city the new restaurant is in and all other required fields. When the user submits this form, the restaurant should appear on the page(unless the view is currently filtered to show a city where the newly restaurant is not in). All fields in the form should also be cleared so that the form is ready for the user to add the next restaurant.
+
+CREATE A FORM TO ALLOW USERS TO ENTER NEW RESTAURANT DATA
+USER'S NEW RESTAURANT DATA SHOULD POPULATE INTO THE JSON
+CLEAR ALL FIELDS AFTER USER ADDS NEW DATA - ENTER KEY AND EVENTLISTENER
+*/
+
+/*
+
 */
